09fff687be2519d0f78e6bd85f6b4fcdf054097b
ElSych <55539754+ElSych@users.noreply.github.com>
2020-03-06 00:41:09 +0300

    Update Flower.cpp

--- Flower.cpp
+++ Flower.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <cmath>
-#include <ctime> 
+#include <ctime>
+#include <SFML/System.hpp>
+#include <string>
 using namespace std;
 
 class cloud{
   float lengh;
   int x;
   int y;
+  int windspeed;
 public:
+  cloud(){
+    х = rand() % 61 - 30;
+    y = 5 + rand() % 11;
+    lengh = y = 5 + rand() % 6;
+    windspeed = rand() % 11 - 5;
+  }
   float getlengh(){
     return lengh;
   }
@@ -22,6 +31,23 @@ public:
   }
   void puty(int y0){
     y=y0;
+  }
+  void cloudposition(){
+    if(windspeed<0){
+      do{
+        x += windspeed;
+      }
+      while (x!=-40);
+      windspeed *= -1;
+    }
+    else{
+      do{
+        x += windspeed;
+      }
+      while (x!=40);
+      windspeed *= -1;
+    }
+   cloudposition();
   }
 };
 class sun{
@@ -44,11 +70,14 @@ public:
   void putrad(int r){
     rad = r;
   }
-  int sunposition(){
+  void sunposition(){
+    for(int i=1; i<180; i++){
+      sleep(1);
+    }
   }
 };
 
-class flower {
+class flower{
   int x;
   int y;
 public:
@@ -69,10 +98,47 @@ public:
     y=y0;
   }
 };
-
+class lamp{
+  string mode;
+public:
+  string getmode(){
+    return mode;
+  }
+  void switchmode(int onoff){
+    if (onoff==1)
+      mode = "ON";
+    else
+      mode = "OFF";
+  }
+  void show(){
+   cout << "Lamp is " << mode << endl; 
+  }
+}
+void lampmode(cloud &c, sun &s, flower &f, lamp &l){
+  int x1 = c.getx();
+  int y1 = y2 = c.gety();
+  int x2 = x1 + c.getlengh();
+  float k = tan (s.getangle()*3.14/180);
+  if(((y1-k*x1)*(y2-k*x2)) <= 0 ){
+    l.switchmode(1);
+  }
+  else
+    l.switchmode(0);
+}
 int main(){
-  cloud cloud;
+  cloud cld;
   sun sun;
-  flower flower;
-  
+  flower flw;
+  lamp lmp;
+  sf::Thread threadsun(&sun::sunposition, &sun);
+  sf::Thread threadcloud(&cloud::cloudposition, &cld);
+  sf::Thread threadlamp(lampmode, cld, sun, flw, lmp);
+  threadsun.launch();
+  threadcloud.launch();
+  threadlamp.launch();
+  do{
+   if ((int t=clock()/CLOCKS_PER_SEC)%10==0)
+     l.show();
+  }
+  while (t=clock()/CLOCKS_PER_SEC<200);
 }
