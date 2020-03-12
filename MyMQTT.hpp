2732c925e1329529e872f1cf12164b860b17a147
ElSych <55539754+ElSych@users.noreply.github.com>
2020-03-12 20:19:39 +0300

    Update MyMQTT.hpp

--- MyMQTT.hpp
+++ MyMQTT.hpp
@@ -6,5 +6,9 @@
 #include "Sun.hpp"
 #include "Flower.hpp"
 #include <thread>
+using namespace MQTTW;
+namespace MQTTFlower{
+    void MQTTFlowerLoop(MyMosq* MQTT)
     void InitMQTT(MyMosq* MQTT);
     void SendData(Lamp* lamp, Sun* sun, MyMosq* MQTT);
+}
