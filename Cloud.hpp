3efc471663318f57360807724734ffb66808dda1
ElSych <55539754+ElSych@users.noreply.github.com>
2020-03-12 19:03:49 +0300

    Create Cloud.hpp

---
+++ Cloud.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <SFML/System.hpp>
+#include <string>
+using namespace std;
+  class cloud{
+    float lengh;
+    int x;
+    int y;
+    int windspeed;
+  public:
+    cloud();
+    float getlengh();
+    float getx();
+    float gety();
+    void putx(int x0);
+    void puty(int y0);
+    void cloudposition();
