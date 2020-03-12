ed9384e015c895de83cb0ddefd42b96cba72786f
ElSych <55539754+ElSych@users.noreply.github.com>
2020-03-12 19:07:56 +0300

    Create Sun.hpp

---
+++ Sun.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <SFML/System.hpp>
+#include <string>
+using namespace std;
+
+class Sun{
+  float angle;
+  int rad;
+public:
+  sun();
+  float getangle();
+  int getrad();
+  void putangle(float a);
+  void putrad(int r);
+  void sunposition();
+};
