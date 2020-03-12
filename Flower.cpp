9694edd52b854e04438313dd8fde2bec2297c38f
ElSych <55539754+ElSych@users.noreply.github.com>
2020-03-12 19:13:49 +0300

    Create Flower.cpp

---
+++ Flower.cpp
@@ -0,0 +1,17 @@
+#include "Flower.hpp"
+    Flower :: Flower(){
+        x = 0;
+        y = 0;
+      }
+      float Flower :: getx(){
+        return x;
+      }
+      float Flower :: gety(){
+        return y;
+      }
+      void Flower :: putx(int x0){
+        x=x0;
+      }
+      void Flower :: puty(int y0){
+        y=y0;
+      }
