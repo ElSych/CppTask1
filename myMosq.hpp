a536e950525f39973c49a5a6bd58a0bf937f6919
ElSych <55539754+ElSych@users.noreply.github.com>
2020-03-12 20:03:26 +0300

    Update myMosq.hpp

--- myMosq.hpp
+++ myMosq.hpp
@@ -4,9 +4,9 @@
 #include <cstring>
 #include <iostream>
 extern std::vector<std::string> mymessages;
-class MosqMqttWrapper : public mosqpp::mosquittopp{
+class MyMosq : public mosqpp::mosquittopp{
   public:
-    MosqMqttWrapper(char const  *id, const char *host, int port);
+    MyMosq(char const  *id, const char *host, int port);
     void on_connect(int rc);
     void on_message(const struct mosquitto_message *message);
     bool send_message(const  char * message);
