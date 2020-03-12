0662624cc0ee8ad169a8af9af6e9ddafc627781f
ElSych <55539754+ElSych@users.noreply.github.com>
2020-03-12 19:14:59 +0300

    Update Sun.hpp

--- Sun.hpp
+++ Sun.hpp
@@ -4,15 +4,14 @@
 #include <SFML/System.hpp>
 #include <string>
 using namespace std;
-
-class Sun{
-  float angle;
-  int rad;
-public:
-  sun();
-  float getangle();
-  int getrad();
-  void putangle(float a);
-  void putrad(int r);
-  void sunposition();
-};
+    class Sun{
+      float angle;
+      int rad;
+    public:
+      sun();
+      float getangle();
+      int getrad();
+      void putangle(float a);
+      void putrad(int r);
+      void sunposition();
+    };
