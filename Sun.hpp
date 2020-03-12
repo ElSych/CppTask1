e7d6d86f695d0fbf90b75d535bac5fc02cda322e
ElSych <55539754+ElSych@users.noreply.github.com>
2020-03-12 20:22:01 +0300

    Update Sun.hpp

--- Sun.hpp
+++ Sun.hpp
@@ -6,12 +6,9 @@
 using namespace std;
     class Sun{
       float angle;
-      int rad;
     public:
       sun();
       float getangle();
-      int getrad();
       void putangle(float a);
-      void putrad(int r);
       void sunposition();
     };
